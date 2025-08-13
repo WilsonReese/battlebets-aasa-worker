4b0c33f658088876cf45c1ae9583756f7ac4d845
Reese Wilson <reese@battlebets.app>
2025-08-13 15:33:08 -0500

    trying out new aasa as host

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
   async fetch(request) {
-    const url = new URL(request.url)
-    const p = url.pathname
+    const { hostname, pathname } = new URL(request.url);
 
-    // match both /.well-known/apple-app-site-association
-    // and /apple-app-site-association (no leading folder)
-    if (
-      p === "/.well-known/apple-app-site-association" ||
-      p === "/apple-app-site-association"
-    ) {
-      const body = JSON.stringify({
-        applinks: {
-          apps: [],
-          details: [
-            {
-              appID: "KZH5U4Z5U3.com.battlebets.battlebets",
-              paths: ["/join", "/join/*", "/join-test/*", "/join-test"],
-            },
-          ],
-        },
-      })
+    const isAASAHost = hostname === "aasa.battlebets.app";
+    const isAASAPath =
+      pathname === "/.well-known/apple-app-site-association" ||
+      pathname === "/apple-app-site-association";
 
-      return new Response(body, {
-        headers: {
-          "Content-Type": "application/json",
-          "Cache-Control": "max-age=3600",
-        },
-      })
-    }
+    if (!isAASAPath) return new Response("Not found", { status: 404 });
 
-    return new Response("Not found", { status: 404 })
-  },
+    const paths = isAASAHost
+      ? [
+          "/join", "/join/*",           // allow deep link handoff here
+          "/join-test", "/join-test/*"  // optional
+        ]
+      : [
+          "NOT /join", "NOT /join/*",   // force browser on join.battlebets.app
+          "/join-test", "/join-test/*"  // optional
+        ];
+
+    const body = JSON.stringify({
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: "KZH5U4Z5U3.com.battlebets.battlebets",
+            paths
+          }
+        ]
+      }
+    });
+
+    return new Response(body, {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "max-age=3600"
+      }
+    });
+  }
 }
